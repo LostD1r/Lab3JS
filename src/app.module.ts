d3cad099de35a792e22092790fb47cce2f556dda
LostD1r <rodina.mark2015@gmail.com>
2022-10-29 13:25:06 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
+import { root } from './utils/paths';
+import { CommentModule } from './comments/comments.module';
+
+console.info(root);
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      entities,
+      logging: true,
+      type: 'sqlite',
+      synchronize: true,
+      database: `${root}/db/db.sqlite`,
+    }),
+    PostModule,
+    CommentModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
